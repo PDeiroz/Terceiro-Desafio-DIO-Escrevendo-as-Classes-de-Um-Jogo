5a63abe78630eb5a9a6f2ddc52cc8e2593496f64
PDeiroz <pdeirozgestao.t.i@outlook.com.br>
2024-01-23 22:16:52 -0300

    Add files via upload

---
+++ Terceiro Desafio Escrevendo as Classes de Um Jogo/src/index.js
@@ -0,0 +1,43 @@
+class Heroi {
+    constructor(nome, idade, tipo) {
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;
+    }
+
+    atacar() {
+        let ataque;
+        switch (this.tipo) {
+            case "mago":
+                ataque = "usou magia";
+                break;
+            case "guerreiro":
+                ataque = "usou espada";
+                break;
+            case "monge":
+                ataque = "usou artes marciais";
+                break;
+            case "ninja":
+                ataque = "usou shuriken";
+                break;
+            default:
+                ataque = "usou um ataque genérico";
+        }
+
+        const mensagem = `O ${this.tipo} ${this.nome} atacou usando ${ataque}`;
+        console.log(mensagem);
+    }
+}
+
+
+const heroi1 = new Heroi("Aragorn", 30, "guerreiro");
+heroi1.atacar();
+
+const heroi2 = new Heroi("Gandalf", 1000, "mago");
+heroi2.atacar();
+
+const heroi3 = new Heroi("Bruce Lee", 40, "monge");
+heroi3.atacar();
+
+const heroi4 = new Heroi("Hanzo", 25, "ninja");
+heroi4.atacar();
